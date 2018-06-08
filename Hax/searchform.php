1ba869e8d3827ee887e3f17c5319c743f00fa920
Laken Hafner <me@laken.me>
2018-06-08 16:23:15 -0400

    Refactored folder structure

--- Hax/searchform.php
+++
@@ -1,3 +0,0 @@
-<form id="fm-search" method="get" action="<?php bloginfo('url'); ?>/" role="search">
-  <p><input type="search" placeholder="Search hacks.mozilla.org" value="<?php the_search_query(); ?>" name="s" id="s" /> <button type="submit">Go</button></p>
-</form>
