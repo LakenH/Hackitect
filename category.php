53b4b26ec45beb328a53a2c08bcd002fbd32b937
Laken Hafner <me@laken.me>
2018-06-09 12:38:03 -0400

    Improve blog templates for usecase

--- category.php
+++ category.php
@@ -1,41 +1,33 @@
-<?php get_header();
-
-// Fetch these IDs now because we'll be using them a lot.
-$demo_id = get_cat_ID('Demo');
-$featureddemo_id = get_cat_ID('Featured Demo');
-$featured_id = get_cat_ID('Featured Article');
-?>
+<?php get_header(); ?>
 
 <main id="content-main" class="section">
-  <?php if (is_category($featured_id)) : ?>
-    <h1 class="page-title">Featured Articles</h1>
-  <?php elseif (is_category($featureddemo_id)) : ?>
-    <h1 class="page-title">Featured Demos</h1>
-  <?php elseif (is_category()) : ?>
-    <h1 class="page-title"><?php single_cat_title(); ?> Articles</h1>
-  <?php else : ?>
-    <h1 class="page-title">Articles</h1>
-  <?php endif; ?>
+    <h1 class="page-title"><?php single_cat_title(); ?></h1>
 
   <?php if (have_posts()) : ?>
   <ul class="article-list">
-    <?php fc_custom_loop($query_string.'&template=article-list'); ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+    <li class="list-item row listing">
+      <?php $authors = get_coauthors($post->ID); ?>
+      <?php echo get_avatar($authors[0], 72) ?>
+      <div class="block block--1">
+        <h3 class="post__title">
+          <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+        </h3>
+        <p class="post__tease"><?php echo get_the_excerpt(); ?></p>
+        <div class="post__meta">
+          Posted on
+          <abbr class="published" title="<?php the_time('Y-m-d\TH:i:sP'); ?>">
+            <?php the_time(get_option('date_format')); ?>
+          </abbr>
+        </div>
+      </div>
+    </li>
+    <?php endwhile;?>
   </ul>
-
+  <?php else: ?>
+    <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+  <?php endif; ?>
   <hr class="dino">
-
-  <?php if (fc_show_posts_nav()) : ?>
-    <?php if (function_exists('fc_pagination') ) : fc_pagination(); else: ?>
-      <nav class="nav-paging">
-        <?php if ( $paged < $wp_query->max_num_pages ) : ?><span class="nav-paging__next"><?php next_posts_link('Next'); ?></span><?php endif; ?>
-        <?php if ( $paged > 1 ) : ?><span class="nav-paging__prev"><?php previous_posts_link('Previous'); ?></span><?php endif; ?>
-      </nav>
-    <?php endif; ?>
-  <?php endif; ?>
-
-  <?php else : ?>
-    <p class="fail">Sorry, there are no articles here.</p>
-  <?php endif; ?>
 </main><!-- /#content-main -->
 
 <?php get_footer(); ?>
