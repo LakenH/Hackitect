07a34972c09b26094687fbe8d1113a4815c5a16e
Laken Hafner <me@laken.me>
2018-06-09 13:09:34 -0400

    Fixing more errors after function purge

--- page-blog.php
+++ page-blog.php
@@ -8,34 +8,42 @@ $categories = array(
   "WordPress"
 );
 ?>
-
-<?php fc_custom_loop('cat='.$featured_id.'&posts_per_page=1&template=home-head-featarticles'); ?>
+<?php query_posts('cat='. $featured_id .'&posts_per_page=1'); ?>
+<?php if (have_posts()): while ( have_posts() ) : the_post(); ?>
+<section
+  class="feature section section--fullwidth">
+  <div class="feature__pane">
+    <h1><a class="feature__link" href="<?php the_permalink() ?>"><?php the_title(); ?>&nbsp;&rarr;</a></h1>
+    <h2 class="feature__label">Featured Article</h2>
+  </div>
+</section>
+<?php endwhile; endif; ?>
 
 <section class="recent content section">
   <div class="row">
     <div class="block block--3">
       <h2 class="heading">Recent Articles</h2>
       <ul class="article-list">
-        <?php fc_custom_loop('posts_per_page=1&template=article-list'); ?>
-      </ul>
-    </div>
-  </div>
-  <div class="row">
-    <div class="block block--2">
-      <ul class="article-list">
-        <?php fc_custom_loop('template=article-list&offset=1'); ?>
-      </ul>
-      <h3 class="read-more"><a href="<?php echo get_permalink(get_page_by_path('articles'));?>page/2/">Browse All Articles &rarr;</a></h3>
-    </div>
-    <div class="block block--1 block--minor">
-      <h2 class="heading">Categories</h2>
-      <ul class="category-list" role="navigation">
-        <?php foreach ($categories as $id) : ?>
-          <?php $cat = get_category(get_cat_ID($id)); ?>
-          <?php if ($cat->cat_ID): ?>
-            <li class="category-list__link"><a href="<?php echo get_category_link($cat->cat_ID); ?>"><?php echo $cat->name; ?></a></li>
-          <?php endif; ?>
-        <?php endforeach; ?>
+        <?php wp_reset_query(); ?>
+        <?php if ( have_posts() ) : ?>
+          <?php while ( have_posts() ) : the_post(); ?>    
+            <li class="list-item row listing">
+              <?php echo get_avatar( get_the_author_meta('user_email'), 72 ); ?>
+              <div class="block block--1">
+                <h3 class="post__title">
+                  <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                </h3>
+                <p class="post__tease"><?php echo get_the_excerpt(); ?></p>
+                <div class="post__meta">
+                  Posted on
+                  <abbr class="published" title="<?php the_time('Y-m-d\TH:i:sP'); ?>">
+                    <?php the_time(get_option('date_format')); ?>
+                  </abbr>
+                </div>
+              </div>
+            </li>
+          <?php endwhile; ?>
+        <?php endif; ?>
       </ul>
     </div>
   </div>
