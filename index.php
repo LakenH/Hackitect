ffab98374563e22e9094a872df870307881db8c4
Laken Hafner <me@laken.me>
2018-06-09 13:12:37 -0400

    fixed blog template

--- index.php
+++ index.php
@@ -1,42 +1,53 @@
-<?php
-// Don't allow direct access to the theme
-if(!defined('DB_NAME')) {
-  exit('Direct template access is not allowed');
-}
-
-get_header();
-
-// Fetch these IDs now because we'll be using them a lot.
-$demo_id = get_cat_ID('Demo');
-$featureddemo_id = get_cat_ID('Featured Demo');
+<?php get_header();
 $featured_id = get_cat_ID('Featured Article');
+$categories = array(
+  "Accessibility",
+  "Extensions",
+  "Plugins",
+  "Themes",
+  "WordPress"
+);
 ?>
+<?php query_posts('cat='. $featured_id .'&posts_per_page=1'); ?>
+<?php if (have_posts()): while ( have_posts() ) : the_post(); ?>
+<section
+  class="feature section section--fullwidth">
+  <div class="feature__pane">
+    <h1><a class="feature__link" href="<?php the_permalink() ?>"><?php the_title(); ?>&nbsp;&rarr;</a></h1>
+    <h2 class="feature__label">Featured Article</h2>
+  </div>
+</section>
+<?php endwhile; endif; ?>
 
-<main id="content-main" class="section">
-  <?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
-  <h1 class="page-title">
-    Articles
-  </h1>
-
-  <?php if (have_posts()) : ?>
-  <ul class="article-list">
-    <?php fc_custom_loop($query_string.'&template=article-list'); ?>
-  </ul>
-
-  <hr class="dino">
-
-  <?php if (fc_show_posts_nav()) : ?>
-    <?php if (function_exists('fc_pagination') ) : fc_pagination(); else: ?>
-      <nav class="nav-paging">
-        <?php if ( $paged < $wp_query->max_num_pages ) : ?><span class="nav-paging__next"><?php next_posts_link('Older'); ?></span><?php endif; ?>
-        <?php if ( $paged > 1 ) : ?><span class="nav-paging__prev"><?php previous_posts_link('Newer'); ?></span><?php endif; ?>
-      </nav>
-    <?php endif; ?>
-  <?php endif; ?>
-
-  <?php else : ?>
-    <p class="fail">Sorry, there are no articles here.</p>
-  <?php endif; ?>
-</main>
+<section class="recent content section">
+  <div class="row">
+    <div class="block block--3">
+      <h2 class="heading">Recent Articles</h2>
+      <ul class="article-list">
+        <?php wp_reset_query(); ?>
+        <?php if ( have_posts() ) : ?>
+          <?php while ( have_posts() ) : the_post(); ?>    
+            <li class="list-item row listing">
+              <?php echo get_avatar( get_the_author_meta('user_email'), 72 ); ?>
+              <div class="block block--1">
+                <h3 class="post__title">
+                  <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                </h3>
+                <p class="post__tease"><?php echo get_the_excerpt(); ?></p>
+                <div class="post__meta">
+                  Posted on
+                  <abbr class="published" title="<?php the_time('Y-m-d\TH:i:sP'); ?>">
+                    <?php the_time(get_option('date_format')); ?>
+                  </abbr>
+                </div>
+              </div>
+            </li>
+          <?php endwhile; ?>
+        <?php endif; ?>
+      </ul>
+    </div>
+  </div>
+</section>
 
 <?php get_footer(); ?>
+
